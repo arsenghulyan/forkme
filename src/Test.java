b9bbe2c95ec568919d1b680feabad65c3e248583
Arsen Ghulyan <arsenghulyan@attask.com>
2012-01-13 17:28:47 +0400

    Test class added

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: arsenghulyan
+ * Date: 1/13/12
+ * Time: 5:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
