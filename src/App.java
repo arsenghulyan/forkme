e4b7133c6b7cabdc58fd1f66f95e15172adb37c0
Arsen Ghulyan <arsenghulyan@attask.com>
2012-01-13 17:49:58 +0400

    local commit\

--- src/App.java
+++ src/App.java
@@ -9,7 +9,7 @@ public class App {
      */
     public static String[] whoForkedThisProject = {
             "Gevorg Harutyunyan",
-            "Unknown Forker"
+            "Unknown Forker GGG"
     };
 
     /**
